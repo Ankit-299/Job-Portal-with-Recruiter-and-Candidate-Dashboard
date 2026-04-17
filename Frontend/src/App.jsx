cd526f7664dd0ab4006f63b7498bfc4ec66723a1
Ankit-299 <bhardwajankit5517@gmail.com>
2026-04-17 13:33:05 +0530

    fixing changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import CandidateDashboard from "./dashboard/CandidateDashboard";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Register from "./pages/register";
 import Applications from "./pages/Applications";
 import ApplicationDetails from "./pages/ApplicationDetails";
 import Profile from "./pages/Profile";
